aec018a1e15bc5dff0f8c4f6f390e1300e516e62
immedha <immedha@outlook.com>
2025-03-15 14:57:57 -0700

    fixed error

--- main.py
+++ main.py
@@ -69,8 +69,8 @@ def gen_podcast_overall(topic):
   print(f"\nCombined audio saved as {output_filename}")
 
   first_audio = AudioSegment.from_file("result.mp3")
-  first_audio = first_audio._spawn(audio.raw_data, overrides={"frame_rate": int(audio.frame_rate * 1.2)})
-  first_audio = first_audio.set_frame_rate(audio.frame_rate)
+  first_audio = first_audio._spawn(first_audio.raw_data, overrides={"frame_rate": int(first_audio.frame_rate * 1.2)})
+  first_audio = first_audio.set_frame_rate(first_audio.frame_rate)
 
   second_audio = AudioSegment.from_file("Standup_comedy.mp3")
 
